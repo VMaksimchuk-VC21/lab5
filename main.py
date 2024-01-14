1eaf6ff9a6791eb43f063078ec6275cee14db732
Валерия Максимчук <VMaksimchuk-VC21@stud.sfu-kras.ru>
2024-01-14 22:58:27 +0700

    first stage

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+import os
+
+
+def main():
+    while True:
+        os.system("cls")
+        print("\nМеню:")
+        print("1. Ввод исходных данных")
+        print("2. Решение задания")
+        print("3. Результат работы алгоритма")
+        print("4. Завершение работы программы")
+
+        point = input("Введите пункт меню: ")
+
+        if point == "1":
+            pass
+        elif point == "2":
+            pass
+        elif point == "3":
+            pass
+        elif point == "4":
+            pass
+        else:
+            pass
+
+
+if __name__ == '__main__':
+    main()
